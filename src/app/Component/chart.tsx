5ea5d11b3f9475a1370627f6f5253bbfefa808db
NGM44 <nikhil@rulezero.com>
2024-03-24 03:56:00 +0530

    Airscale

--- src/app/Component/chart.tsx
+++ src/app/Component/chart.tsx
@@ -5,8 +5,30 @@ import React from "react";
 const BarChart = () => {
   const option = {
     xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      type: "category",
+      data: [
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat",
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat",
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat",
+        "Sun",
+      ],
     },
     yAxis: {
       type: "value",
@@ -23,14 +45,14 @@ const BarChart = () => {
       z: 10,
     },
 
-      tooltip: {
-      trigger: 'axis',
+    tooltip: {
+      trigger: "axis",
       axisPointer: {
-        type: 'cross',
+        type: "cross",
         label: {
-          backgroundColor: '#6a7985'
-        }
-      }
+          backgroundColor: "#6a7985",
+        },
+      },
     },
     grid: {
       top: 110,
@@ -41,18 +63,31 @@ const BarChart = () => {
     series: [
       {
         color: "#69B2F8",
-        backgroundColor : "#D1E6FA",
-        data: [120, 200, 150, 80, 70, 110, 130,120, 200, 150, 80, 70, 110, 130,120, 200, 150, 80, 70, 110, 130],
-        type: 'bar'
-      }
-    ]
+        backgroundColor: "#D1E6FA",
+        data: [
+          120, 200, 150, 80, 70, 110, 130, 120, 200, 150, 80, 70, 110, 130, 120,
+          200, 150, 80, 70, 110, 130,
+        ].map((ele) => {
+          const val = _.random(150, 300);
+          return {
+            value: val,
+            itemStyle: {
+              color: val > 270 ? "#e62d2d" : "#69B2F8",
+            },
+          };
+        }),
+        type: "bar",
+      },
+    ],
   };
- 
+
   return (
-    <div className="bg-white border border-gray-200 shadow-md rounded-md px-4 py-2" >
+    <div className="bg-white border border-gray-200 shadow-md rounded-md px-4 py-2">
       <div>
         <p className="font-medium">AQI Reading</p>
-        <p className="text-xs text-gray-500">AQI is pollutant in air which level should not cross 100</p>
+        <p className="text-xs text-gray-500">
+          Value of AQI Above 250 is Considered as danger
+        </p>
       </div>
       <ReactECharts option={option} />
     </div>
